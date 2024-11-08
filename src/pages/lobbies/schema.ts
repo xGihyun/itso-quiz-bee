b6f8d802971ce5b047e6bc74a6298d481215f8b5
xGihyun <giordnnuz@gmail.com>
2024-11-08 09:21:14 +0800

    Migrated from Astro to React + TanStack

--- src/pages/lobbies/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "astro/zod";
-
-export const JoinLobbySchema = z.object({
-  //user_id: z.string().min(1, { message: "Required" }),
-  code: z.string().min(1, { message: "Required" }),
-});
-
-export type JoinLobbyInput = z.infer<typeof JoinLobbySchema>;
--- src/pages/login/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "astro/zod";
-
-export const LoginSchema = z.object({
-  email: z.string().email().min(1, { message: "Required" }),
-  password: z.string().min(1, { message: "Required" }),
-});
-
-export type LoginInput = z.infer<typeof LoginSchema>
