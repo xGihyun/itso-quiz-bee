faae18a723f3229fa0c3705ab87e489429df75c8
xGihyun <giordnnuz@gmail.com>
2024-11-09 13:42:01 +0800

    Working on adding quizzes

--- src/routes/quizzes/$quizId/index.tsx
+++ src/routes/quizzes/$quizId/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/quizzes/$quizId/")({
@@ -5,5 +6,17 @@ export const Route = createFileRoute("/quizzes/$quizId/")({
 });
 
 function RouteComponent(): JSX.Element {
-  return <div>Specific Quiz!</div>;
+
+  // TODO: Use form to dynamically add questions and stuff
+  return (
+    <div>
+      <div>
+        <h1>Untitled Quiz</h1>
+
+        <p>Insert description</p>
+      </div>
+
+      <Button>Add Question</Button>
+    </div>
+  );
 }
--- src/routes/quizzes/index.tsx
+++ src/routes/quizzes/index.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { v4 as uuidv4 } from "uuid";
 
 export const Route = createFileRoute("/quizzes/")({
   component: RouteComponent,
 });
 
 function RouteComponent(): JSX.Element {
-  return <div>Quiz!</div>;
+  const navigate = useNavigate({ from: "/quizzes" });
+
+  const createQuiz = async (): Promise<void> => {
+    const quizId = uuidv4();
+
+    await navigate({ to: "/quizzes/$quizId", params: { quizId: quizId } });
+  };
+
+  return (
+    <div>
+      <Button onClick={createQuiz}>Create</Button>
+    </div>
+  );
 }
