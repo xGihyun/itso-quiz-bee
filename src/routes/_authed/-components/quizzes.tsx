f9c1121017bc13c27e1380489ca7b2984d17f116
xGihyun <giordnnuz@gmail.com>
2025-04-14 15:21:35 +0800

    feat(quiz): remove websocket connection on `quizzes.tsx`

--- src/routes/_authed/-components/quizzes.tsx
+++ src/routes/_authed/-components/quizzes.tsx
@@ -5,14 +5,11 @@ import {
 	CardTitle
 } from "@/components/ui/card";
 import { useNavigate } from "@tanstack/react-router";
-import { JSX, useEffect, useRef } from "react";
-import gsap from "gsap";
+import { JSX } from "react";
 import { useAuth } from "@/auth";
 import { QuizBasicInfo } from "@/lib/quiz/types";
 import { UserRole } from "@/lib/user";
-import useWebSocket from "react-use-websocket";
 import { WebSocketEvent, WebSocketRequest } from "@/lib/websocket/types";
-import { WEBSOCKET_OPTIONS, WEBSOCKET_URL } from "@/lib/websocket/constants";
 
 type Props = {
 	quizzes: QuizBasicInfo[];
@@ -24,30 +21,8 @@ type JoinQuizRequest = {
 };
 
 export function Quizzes(props: Props): JSX.Element {
-	const socket = useWebSocket(WEBSOCKET_URL, WEBSOCKET_OPTIONS);
 	const auth = useAuth();
 	const navigate = useNavigate({ from: "/" });
-	const quizzesRef = useRef<HTMLDivElement | null>(null);
-
-	useEffect(() => {
-		setTimeout(() => {
-			if (!quizzesRef.current) {
-				return;
-			}
-
-			gsap.fromTo(
-				quizzesRef.current.children,
-				{ opacity: 0, y: 20 },
-				{
-					opacity: 1,
-					y: 0,
-					duration: 0.6,
-					stagger: 0.1,
-					ease: "power2.out"
-				}
-			);
-		}, 1000);
-	}, []);
 
 	async function joinQuiz(quiz: QuizBasicInfo) {
 		if (auth.user === null) {
@@ -71,11 +46,10 @@ export function Quizzes(props: Props): JSX.Element {
 			event: WebSocketEvent.PlayerJoin,
 			data: { quizId: quiz.quizId, userId: auth.user.userId }
 		};
-		socket.sendJsonMessage(message);
 	}
 
 	return (
-		<div className="grid grid-cols-4 gap-4 py-4" ref={quizzesRef}>
+		<div className="grid grid-cols-4 gap-4 py-4">
 			{props.quizzes.map((quiz) => (
 				<button
 					key={quiz.quizId}
