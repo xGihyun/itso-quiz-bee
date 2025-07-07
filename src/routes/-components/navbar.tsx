4e81b263f2b37ba7271b6f15f4c5bc195950a80e
xGihyun <giordnnuz@gmail.com>
2025-07-07 09:01:46 +0800

    feat: navigate to `/sign-up` on sign out

--- src/routes/-components/navbar.tsx
+++ src/routes/-components/navbar.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/auth";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { JSX } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import itsoUmakLogo from "../-images/itso-umak-logo.webp";
 
 export function Navbar(): JSX.Element {
 	const auth = useAuth();
+	const navigate = useNavigate();
+
+	async function handleSignOut(): Promise<void> {
+		await auth.signOut();
+		await navigate({ to: "/sign-in" });
+	}
 
 	return (
 		<nav className="fixed inset-0 z-[998] flex h-16 w-full items-center justify-between border-b border-b-border bg-card px-10">
 			<div className="h-full py-2">
-				<Link href="/" className="h-full content-center">
+				<Link to="/" className="h-full content-center">
 					<img
 						src={itsoUmakLogo}
 						alt="ITSO Logo"
@@ -21,7 +27,7 @@ export function Navbar(): JSX.Element {
 			</div>
 
 			<div className="flex items-center gap-2">
-				<Button onClick={auth.signOut}>Logout</Button>
+				<Button onClick={handleSignOut}>Logout</Button>
 
 				<Avatar>
 					<AvatarImage src="https://github.com/shadcn.png" />
